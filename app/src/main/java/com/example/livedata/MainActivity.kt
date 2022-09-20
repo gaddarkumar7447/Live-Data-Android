f19c8cb517b6af9ef02e078407f04cb66c570515
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-20 18:42:37 +0530

    some changes

--- app/src/main/java/com/example/livedata/MainActivity.kt
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.livedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding  : ActivityMainBinding
+    private lateinit var modelClass: ViewModelClass
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        modelClass = ViewModelProvider(this)[ViewModelClass ::class.java]
+
+        modelClass.total.observe(this) {
+            binding.textShow.text = it.toString()
+        }
+
+
+        binding.button.setOnClickListener(View.OnClickListener {
+            modelClass.getTotal(binding.editText.text.toString().toInt())
+
+        })
     }
 }
